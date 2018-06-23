1ad0f861fceeed27bd4a884845e37748f4558fa8
rahul <officialrahulmandal@gmail.com>
2018-06-23 21:12:51 +0530

    Homepage Created

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+class Main extends Component {
+	render()  {
+		return (
+			<div>
+			  <h1>Welcome to Influence app.</h1>
+			  <ul>
+			    <li><a href="/">Home</a></li>
+			    <li><a href="Login">Log-In</a></li>
+			    <li><a href="register">Register</a></li>
+			  </ul>
+			</div>
+			  );
+
+	}
+}
+
+export default Main;
