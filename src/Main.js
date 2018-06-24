740b9b045f0db503e5f06bc1a3ef628a373c3a41
rahul <officialrahulmandal@gmail.com>
2018-06-24 16:46:22 +0530

    Navigation bar created

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import Login from "./login";
+import Register from "./registration";
+import Home from "./Home";
 
 
 class Main extends Component {
 	render()  {
 		return (
-			<div>
-			  <h1>Welcome to Influence app.</h1>
-			  <ul>
-			    <li><a href="/">Home</a></li>
-			    <li><a href="Login">Log-In</a></li>
-			    <li><a href="register">Register</a></li>
-			  </ul>
-			</div>
+			<HashRouter>
+        <div>
+          <h1>Welcome to Influence app.</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Login</NavLink></li>
+            <li><NavLink to="/contact">Register</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Login}/>
+            <Route path="/contact" component={Register}/>
+          </div>
+        </div>
+      </HashRouter>
 			  );
 
 	}
---
+++ src/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Log-In Page</h2>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
